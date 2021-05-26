7c81bd41d84664f2fc064f91de0eab7d3f33c34e
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-05-26 11:08:08 +0200

    using default translations for everyone based on navigator.language

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import Amplify, { Auth, Hub } from 'aws-amplify';
+import { Translations } from '@aws-amplify/ui-components';
+import Amplify, { Auth, Hub, I18n } from 'aws-amplify';
 import '@aws-amplify/ui-vue';
 import Vue from 'vue';
 import App from './App.vue';
@@ -10,6 +11,12 @@ import store from './store';
 Vue.config.productionTip = false;
 
 Amplify.configure(awsExports);
+
+I18n.putVocabulariesForLanguage(navigator.language, {
+  [Translations.CONFIRM_TOTP_CODE]: 'Enter code from your Authenticator App',
+  [Translations.CODE_LABEL]: 'CODE_LABEL 2',
+  [Translations.CODE_PLACEHOLDER]: 'CODE_PLACEHOLDER 2',
+});
 
 new Vue({
   router,
